6140c5354925a2a0cc310c560403057f5b880c23
Drew Short <nerian.drew@gmail.com>
2014-05-07 17:41:52 -0500

    Added CDN repositories as the first location to request the large libraries. Made the included libraries the fallback version.

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,14 @@ require.config({
   },
   paths: {
       // Library Specific Paths
-      "backbone":     "lib/backbonejs/backbone-min", // Version:
-      "underscore":   "lib/underscorejs/underscore-min", // Version:
-      "jquery":       "lib/jQuery/jQuery", // Version:
-      "bootstrap":    "lib/bootstrap/bootstrap.min", // Version:
-      "doTCompiler":  "lib/doT/doT", // Version:
-      "text":         "lib/requirejs-text/text", // Version:
-      "doT":          "lib/requirejs-doT/doT" // Version:
+      // First load from CDN if available, then fallback to local copies
+      "backbone":     ["//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min", "lib/backbonejs/backbone-min"], // Version: 1.1.2 From: http://backbonejs.org/
+      "underscore":   ["//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min", "lib/underscorejs/underscore-min"], // Version: 1.6.0 From: http://underscorejs.org/
+      "jquery":       ["//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min", "lib/jQuery/jQuery"], // Version: 2.1.1 From: http://jquery.com/
+      "bootstrap":    ["//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min", "lib/bootstrap/bootstrap.min"], // Version: 3.1.1 From: http://getbootstrap.com/
+      "doTCompiler":  ["lib/doT/doT"], // Version: 1.0.0 From: http://olado.github.io/doT/index.html
+      "text":         ["//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text", "lib/requirejs-text/text"], // Version: 2.0.10 From: https://github.com/requirejs/text
+      "doT":          ["lib/requirejs-doT/doT"] // Version: 0.1.2. From: https://github.com/ullmark/requirejs-doT
   },
   doT: {
     ext: '.dot', // extension of the templates, defaults to .dot
