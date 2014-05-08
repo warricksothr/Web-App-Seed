c8c3f982e27e9e56ee71fedfb277124aabe40ff6
Drew Short <nerian.drew@gmail.com>
2014-05-08 06:08:51 -0500

    Cleanup, formatting and some data changes

--- assets/js/app/interface/draw_header.js
+++ assets/js/app/interface/draw_header.js
@@ -1,7 +1,7 @@
-define(["doT!template/header", "text!app/app-data.js"], function(tmpl, data_text) {
-  //return as a function that can be called to display the header
-  return function() {
-    var data = JSON.parse(data_text)
-    $("#header")[0].innerHTML = tmpl(data);
-  };
+define(["doT!template/header", "text!app/app-data.json"], function (tmpl, data_text) {
+    //return as a function that can be called to display the header
+    return function () {
+        var data = JSON.parse(data_text)
+        $("#header")[0].innerHTML = tmpl(data);
+    };
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,53 @@
 //Require Configuration
 require.config({
-  baseUrl: "assets/js",
-  // Break caching (DEVELOPMENTAL SETTING)
-  urlArgs: "bust=" + (new Date()).getTime(),
-  //Shim for non AMD libraries
-  shim: {
-    underscore: {
-      exports: '_'
+    baseUrl: "assets/js",
+    // Break caching (DEVELOPMENTAL SETTING)
+    urlArgs: "bust=" + (new Date()).getTime(),
+    //Shim for non AMD libraries
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+        bootstrap: {
+            deps: ["jquery"]
+        }
     },
-    backbone: {
-      deps: ["underscore", "jquery"],
-      exports: "Backbone"
+    paths: {
+        // Library Specific Paths
+        // First load from CDN if available, then fallback to local copies
+        "backbone": ["//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min", "lib/backbonejs/backbone-min"], // Version: 1.1.2 From: http://backbonejs.org/
+        "underscore": ["//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min", "lib/underscorejs/underscore-min"], // Version: 1.6.0 From: http://underscorejs.org/
+        "jquery": ["//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min", "lib/jQuery/jQuery"], // Version: 2.1.1 From: http://jquery.com/
+        "bootstrap": ["//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min", "lib/bootstrap/bootstrap.min"], // Version: 3.1.1 From: http://getbootstrap.com/
+        "doTCompiler": ["lib/doT/doT"], // Version: 1.0.0 From: http://olado.github.io/doT/index.html
+        "text": ["//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text", "lib/requirejs-text/text"], // Version: 2.0.10 From: https://github.com/requirejs/text
+        "doT": ["lib/requirejs-doT/doT"] // Version: 0.1.2. From: https://github.com/ullmark/requirejs-doT
     },
-    bootstrap: {
-      deps: ["jquery"]
-    }
-  },
-  paths: {
-      // Library Specific Paths
-      // First load from CDN if available, then fallback to local copies
-      "backbone":     ["//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min", "lib/backbonejs/backbone-min"], // Version: 1.1.2 From: http://backbonejs.org/
-      "underscore":   ["//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min", "lib/underscorejs/underscore-min"], // Version: 1.6.0 From: http://underscorejs.org/
-      "jquery":       ["//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min", "lib/jQuery/jQuery"], // Version: 2.1.1 From: http://jquery.com/
-      "bootstrap":    ["//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min", "lib/bootstrap/bootstrap.min"], // Version: 3.1.1 From: http://getbootstrap.com/
-      "doTCompiler":  ["lib/doT/doT"], // Version: 1.0.0 From: http://olado.github.io/doT/index.html
-      "text":         ["//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text", "lib/requirejs-text/text"], // Version: 2.0.10 From: https://github.com/requirejs/text
-      "doT":          ["lib/requirejs-doT/doT"] // Version: 0.1.2. From: https://github.com/ullmark/requirejs-doT
-  },
-  doT: {
-    ext: '.dot', // extension of the templates, defaults to .dot
-    templateSettings: {
-      evaluate:    /\{\{([\s\S]+?)\}\}/g,
-      interpolate: /\{\{=([\s\S]+?)\}\}/g,
-      encode:      /\{\{!([\s\S]+?)\}\}/g,
-      use:         /\{\{#([\s\S]+?)\}\}/g,
-      define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
-      conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
-      iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
-      varname: 'it',
-      strip: true,
-      append: true,
-      selfcontained: false
-    }
-  },
-  waitSeconds: 15
+    doT: {
+        ext: '.dot', // extension of the templates, defaults to .dot
+        templateSettings: {
+            evaluate: /\{\{([\s\S]+?)\}\}/g,
+            interpolate: /\{\{=([\s\S]+?)\}\}/g,
+            encode: /\{\{!([\s\S]+?)\}\}/g,
+            use: /\{\{#([\s\S]+?)\}\}/g,
+            define: /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
+            conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
+            iterate: /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
+            varname: 'it',
+            strip: true,
+            append: true,
+            selfcontained: false
+        }
+    },
+    waitSeconds: 15
 });
 
 //Kickstart the app
 //Loads libraries as required and even shims things nicely
-require(["app/app"], function(app) {
-  app.init()
+require(["app/app"], function (app) {
+    app.init();
 });
